0889054f1a1305019d43938b3bb8de06056c7e75
Philip Cristobal <philipcristobal06@gmail.com>
2023-12-01 09:17:45 +0800

    Change title

---
+++ src/components/Hello.js
@@ -0,0 +1,6 @@
+//const name = "Philip";
+const displayMessage = () => "Hello World";
+const Hello = () => <h1>The message is: {displayMessage()}</h1>;
+
+
+export default Hello;
---
+++ src/components/Message.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+class Message extends Component{
+    //class must have render method
+    render() {
+        return <h1>This is a class component</h1>; 
+    }
+}
+
+export default Message;
